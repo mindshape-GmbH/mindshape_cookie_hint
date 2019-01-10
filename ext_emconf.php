2a67714361554ab27afb46eb99b1f90490b45825
Can Karadag <karadag@mindshape.de>
2019-01-10 09:18:03 +0100

    [TASK] Add support for TYPO3 9.5.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -38,10 +38,9 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company' => 'mindshape GmbH',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-8.7.99',
+            'typo3' => '6.2.0-9.5.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
     ),
 );
-
